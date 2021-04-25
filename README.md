57c3fb11021b21cca098ffed20ab1de49df98bde
urimem <uri.mailbox@gmail.com>
2021-04-26 00:54:45 +0300

    MongoDB stuff added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Data Access Layer implementation.
 
 Using MongoDB with Atlas Sample data as data-source.
+
+Future: add Java Logger with file handler
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,18 +1,47 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ConnectionString;
 import com.mongodb.ServerAddress;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
+import org.bson.Document;
+
 
 import java.util.Arrays;
 
 public class HelloWorld {
+
+    private Properties prop;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        MongoClient mongoClient = MongoClients.create("mongodb+srv://<username>:<password>@cluster0.o7p2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        Properties prop = new Properties();
+        try {
+            // load a properties file for reading
+            prop.load(new FileInputStream("local.properties"));
+            // get the properties and print
+            prop.list(System.out);
+            //Reading each property value
+            //System.out.println(prop.getProperty("FileName"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
+        System.out.println(prop.getProperty("mongo.connectionString"));
+
+        MongoClient mongoClient = MongoClients.create(prop.getProperty("mongo.connectionString"));
+        MongoDatabase database = mongoClient.getDatabase(prop.getProperty("mongo.dbname"));
+        MongoCollection<Document> collection = database.getCollection(prop.getProperty("mongo.collectionName"));
+
+        System.out.println("No' of docs in this collection: " + collection.countDocuments());
+        mongoClient.close();
     }
 }
